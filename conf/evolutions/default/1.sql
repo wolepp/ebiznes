184360f7349f4c659508dbe9c540416a7696b134
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2021-05-09 19:51:59 +0200

    Some updates in configuration

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,125 @@
+-- !Ups
+
+CREATE TABLE "cart"
+(
+    "id"         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id"    INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "discount"   REAL    NOT NULL,
+    "quantity"   INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
+
+CREATE TABLE "category"
+(
+    "id"                 INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name"               VARCHAR NOT NULL,
+    "parent_category_id" INTEGER,
+    FOREIGN KEY (parent_category_id) REFERENCES "category" (id)
+);
+
+CREATE TABLE "delivery"
+(
+    "id"              INTEGER   NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "status"          INTEGER   NOT NULL,
+    "shipping_method" INTEGER   NOT NULL,
+    "delivery_date"   TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "orderitem"
+(
+    "id"         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "order_id"   INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES "order" (id),
+    FOREIGN KEY (product_id) REFERENCES "product" (id)
+);
+
+CREATE TABLE "order"
+(
+    "id"               INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id"          INTEGER,
+    "delivery_id"      INTEGER,
+    "special_offer_id" INTEGER,
+    "payment_id"       INTEGER,
+    "sum"              REAL    NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (delivery_id) REFERENCES "delivery" (id),
+    FOREIGN KEY (special_offer_id) REFERENCES "specialoffer" (id),
+    FOREIGN KEY (payment_id) REFERENCES "payment" (id)
+);
+
+CREATE TABLE "payment"
+(
+    "id"         INTEGER   NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "status"     INTEGER   NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "product"
+(
+    "id"          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "category_id" INTEGER NOT NULL,
+    "name"        VARCHAR NOT NULL,
+    "description" VARCHAR NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES "category" (id)
+);
+
+CREATE TABLE "return"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id"  INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
+    "status"   INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (order_id) REFERENCES "order" (id)
+);
+
+CREATE TABLE "specialoffer"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name"     VARCHAR NOT NULL,
+    "discount" REAL    NOT NULL
+);
+
+CREATE TABLE "user"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name"     VARCHAR NOT NULL,
+    "email"    VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "city"     VARCHAR NOT NULL,
+    "address"  VARCHAR NOT NULL
+);
+
+CREATE TABLE "wishlistitem"
+(
+    "id"          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "wishlist_id" INTEGER NOT NULL,
+    "product_id"  INTEGER NOT NULL,
+    FOREIGN KEY ("wishlist_id") REFERENCES "wishlist" (id),
+    FOREIGN KEY ("product_id") REFERENCES "product" (id)
+);
+
+CREATE TABLE "wishlist"
+(
+    "id"      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "name"    VARCHAR NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "user" (id)
+);
+
+-- !Downs
+
+DROP TABLE "cart";
+DROP TABLE "category";
+DROP TABLE "delivery";
+DROP TABLE "orderitem";
+DROP TABLE "order";
+DROP TABLE "payment";
+DROP TABLE "product";
+DROP TABLE "return";
+DROP TABLE "specialoffer";
+DROP TABLE "user";
+DROP TABLE "wishlistitem";
+DROP TABLE "wishlist";
